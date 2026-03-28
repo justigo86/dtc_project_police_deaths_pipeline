3b0523ebcf059e424946350a8bac48b797a74c15
justigo86 <justigo86@gmail.com>
2026-03-28 02:10:14 -0400

    added cause mapping seed file, staging file, intermediate file, and packages file to pull packages into docker container - verified staging model

---
+++ 03_dbt_project/target/compiled/police_deaths_pipeline/models/01_staging/stg_police_deaths.sql
@@ -0,0 +1,30 @@
+WITH source AS (
+    SELECT * FROM `zoomcamp-final-project-491003`.`proj_bq_dataset`.`raw_police_deaths`
+),
+
+renamed AS (
+    SELECT
+        -- Primary Key (Composite)
+        to_hex(md5(cast(coalesce(cast(Name as string), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(End_Of_Watch as string), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(Department as string), '_dbt_utils_surrogate_key_null_') as string))) AS death_id,
+        
+        -- Core Info
+        CAST(Rank AS STRING) AS rank,
+        CAST(Name AS STRING) AS officer_name,
+        CAST(Age AS FLOAT64) AS age, 
+        
+        -- Casting DATETIME to DATE for partitioning
+        CAST(End_Of_Watch AS DATE) AS end_of_watch_date,
+        
+        CAST(Department AS STRING) AS department,
+        CAST(State AS STRING) AS state,
+        
+        -- Incident Info
+        CAST(Cause AS STRING) AS cause_raw,
+        CAST(Weapon AS STRING) AS weapon,
+        CAST(Offender AS STRING) AS offender_status,
+        CAST(Summary AS STRING) AS incident_summary
+
+    FROM source
+)
+
+SELECT * FROM renamed
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ## DataTalksClub Data Engineering Zoomcamp 2026 Final Project
 
+# Questions to answer from dataset:
+1. "How has the risk profile of American law enforcement shifted over the years when comparing 'Intentional/Violent' causes of death vs. 'Accidental/Environmental' causes?"
+2. Assessing violent vs non-violent percentages per decade
+3. Assess what types of deaths are more likely based on day of the week
+
 # Reproducibility
 Steps:
 1. Clone the repo
