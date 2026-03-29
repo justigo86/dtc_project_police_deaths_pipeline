80098cc8119984ec1bcbf0cc0e11577241e90d36
justigo86 <justigo86@gmail.com>
2026-03-28 23:14:16 -0400

    updated seed data and verified int model/tables through querying

--- 03_dbt_project/models/02_intermediate/int_police_deaths.sql
+++ 03_dbt_project/models/02_intermediate/int_police_deaths.sql
@@ -11,10 +11,11 @@ joined AS (
         s.*,
         -- 'Other/Unknown' if seed data null or missing
         COALESCE(c.risk_category, 'Other/Unknown') AS risk_category,
+        -- incident year vs.
         EXTRACT(YEAR FROM s.end_of_watch_date) AS incident_year,
-        -- Calculating decades for violence percentages
+        -- incident decade for violence percentages
         FLOOR(EXTRACT(YEAR FROM s.end_of_watch_date) / 10) * 10 AS incident_decade,
-        -- Day of week col
+        -- day of week col to ensure correct day being used
         FORMAT_DATE('%A', s.end_of_watch_date) AS day_of_week_name
     FROM staging AS s
     LEFT JOIN categories AS c
